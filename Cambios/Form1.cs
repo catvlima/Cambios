ef496178b0f753e68c310ee34d41beaba23c6b51
catvlima <cata.lima97@hotmail.com>
2021-05-22 18:49:04 +0100

    LoadRates

--- Cambios/Form1.cs
+++ Cambios/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Cambios.Modelos;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Cambios
 {
     public partial class Form1 : Form
@@ -16,9 +19,30 @@ namespace Cambios
         public Form1()
         {
             InitializeComponent();
-
+            LoadRates();
         }
 
-       
+        private async void LoadRates()
+        {
+            //bool load;
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://cambios.somee.com");
+
+            var response = await client.GetAsync("/api/rates");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                MessageBox.Show(response.ReasonPhrase);
+                return;
+            }
+
+            var rates = JsonConvert.DeserializeObject<List<Rate>>(result);
+
+            comboBoxOrigem.DataSource = rates;
+
+        }
     }
 }
--- Cambios/Modelos/Rate.cs
+++ Cambios/Modelos/Rate.cs
@@ -8,6 +8,9 @@ namespace Cambios.Modelos
 {
     public class Rate
     {
-       
+        public int RateId { get; set; }
+        public string Code { get; set; }
+        public double TaxRate { get; set; }
+        public string Name { get; set; }
     }
 }
